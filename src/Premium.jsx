e6329cd4f814a2c743da49438d7f89e63dfaf39f
Debashish <debashish.panigrahi@havells.com>
2025-03-18 18:39:32 +0530

    improved performace

--- src/Premium.jsx
+++ src/Premium.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState , useCallback} from 'react'
 import axios from "axios";
 import { BASE_URL } from "./utils/constant";
-import { CheckCircleIcon } from "lucide-react";
+import { CheckCircleIcon , Loader2} from "lucide-react";
 
 function Premium() {
     const [isUserPremium,setUserPremium] = useState(false)
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         verifyPremiumMember()
@@ -45,17 +46,29 @@ function Premium() {
         rzp.open();
     }
 
-    const verifyPremiumMember = async () => {
-        const res = await axios.get(BASE_URL + "premium/verify", {
-            withCredentials: true,
-          });
-          console.log('front end' , res.data)
-          console.log('front end data' , res.data.isPremium)
-          if (res.data.isPremium) {
-            setUserPremium(true);
-          }
-    }
+    const verifyPremiumMember = useCallback(async () => {
+        try{
+            setIsLoading(true);
+            const res = await axios.get(BASE_URL + "premium/verify", {
+                withCredentials: true,
+              });
+              setUserPremium(res.data.isPremium);
+        }catch(error){
+            console.error("Verification failed", error);
+        }finally{
+            setIsLoading(false);
+        }
+       
+    }, [])
   return(
+  isLoading ? 
+  <div className="flex flex-col items-center justify-center min-h-screen bg-transparent text-white p-6">
+  <div className="flex flex-col items-center">
+    <Loader2 size={48} className="animate-spin text-white" />
+    <p className="mt-4">Checking membership status...</p>
+  </div>
+</div>
+   :
     isUserPremium ? <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-green-400 to-blue-500 text-white p-6">
     <div className="bg-white text-gray-900 p-8 rounded-2xl shadow-xl max-w-md text-center">
       <CheckCircleIcon size={64} className="text-green-500 mx-auto mb-4" />
@@ -105,7 +118,9 @@ function Premium() {
       onClick={() => handleBuyClick("platinum")}
       className="btn btn-accent">Buy Platinum</button>
     </div>
-  </div>
+    </div>
+    
+  
   
   );
 }
